be2af656d699e8e4e69dce74b817bf52fc57246f
Brad Pinter <brad.pinter@gmail.com>
2015-07-01 10:34:05 -0400

    cli-starter

---
+++ cli-starter/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+source /build_environment.sh
+
+# Compile statically linked version of package
+echo "Building $pkgName"
+`CGO_ENABLED=${CGO_ENABLED:-0} go build -a --installsuffix cgo --ldflags="${LDFLAGS:--s}" $pkgName`
+
+# Grab the last segment from the package name
+name=${pkgName##*/}
+
+if [[ $COMPRESS_BINARY == "true" ]];
+then
+  goupx $name
+fi
+
+if [ -e "/var/run/docker.sock" ] && [ -e "./Dockerfile" ];
+then
+  # Default TAG_NAME to package name if not set explicitly
+  tagName=${tagName:-"$name":latest}
+
+  # Build the image from the Dockerfile in the package directory
+  docker build -t $tagName .
+fi
